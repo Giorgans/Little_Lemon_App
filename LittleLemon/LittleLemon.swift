1ff1e2a8bf38bb4ca764b029e56c38a928e820c9
George Zervos <g.zervos96@windowslive.com>
2024-02-07 01:25:15 +0200

    Initial Commit

---
+++ LittleLemon/LittleLemon.swift
@@ -0,0 +1,13 @@
+
+import SwiftUI
+
+@main
+struct LittleLemon: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# littlelemon
+### Little Lemon Restaurant iOS App
+## Asignment for iOS App Capstone course  by Meta, on coursera course platform.
+Made with SwiftUI iOS 17.2 , mac OS Sonoma 14.3
